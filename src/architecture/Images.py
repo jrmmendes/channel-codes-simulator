0c80bda3894d51c47b981ba91f12230570a6a346
jrmmendes <jrmmendes@outlook.com>
2017-12-09 20:35:07 -0300

    Adicionada estrutura de matrizes

--- src/architecture/Images.py
+++ src/architecture/Images.py
@@ -4,16 +4,25 @@ import src.misc.exceptions as exception
 class MatrixBasedImage:
     """Representação de imagens por matrizes RGB"""
 
-    def __init__(self, rows, collumns):
+    def __init__(self, rows, col):
         if rows <= 0:
             raise exception.SizeError("rows")
 
-        if collumns <= 0:
+        if col <= 0:
             raise exception.SizeError("collumns")
 
         self.__rows__ = rows
-        self.__collumns__ = collumns
+        self.__col__ = col
+        self.__red__ = np.zeros((rows, col), 'int8')
+        self.__green__ = np.zeros((rows, col), 'int8')
+        self.__blue__ = np.zeros((rows, col), 'int8')
+        self.__colors__ = (self.__red__, self.__green__, self.__blue__)
 
-        """Red = 0, Green = 1, Blue = 2"""
-        self.__colors__ = np.zeros((rows, collumns), 3)
+    def getRedMatrix(self):
+        return self.__colors__[0]
 
+    def getGreenMatrix(self):
+        return self.__colors__[1]
+
+    def getBlueMatrix(self):
+        return self.__colors__[2]
